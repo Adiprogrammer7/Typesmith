d844e06dd61932ae1112be12e30dbc9acc10dd0b
Aaditya Tile <adi01tile@gmail.com>
2022-02-09 22:53:54 +0530

    dynamic text done

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const p = document.getElementById("para");
 const blacklist_chars = ['Shift', 'CapsLock', 'Control', 'Alt', 'Meta', 'Backspace', 'Enter'];
-
-// initialization to the first char of typing text
-let cursor_index = 0;
-let current_char;
+let current_char, para_text, para_len, cursor_index;
 let isRight = true;
-let para_text;
 
-// to fetch text from api
+// async code to fetch text from api
 const getData = async () => {
 	const response = await fetch('https://api.quotable.io/random?minLength=100&maxLength=600');
 	const data = await response.json();
@@ -16,7 +12,7 @@ const getData = async () => {
 getData().then((data) => {  //when promise is resolved
 	para_text = data['content'];
 	span_chars(para_text);
-	console.log(data['content']);
+	console.log(para_text);
 }).catch((e) => {  //when error occurs
 	para_text = "its some error text";
 	span_chars(para_text);
@@ -25,12 +21,16 @@ getData().then((data) => {  //when promise is resolved
 
 // create span for each char and add it to html paragraph element
 function span_chars(input_text){
+	p.innerHTML = "";
+	cursor_index = 0;
+	para_len = input_text.length;
 	const chars = input_text.split("");
 	for(let i = 0; i < chars.length; i++){
 		let span_tag = document.createElement('span');
 		span_tag.innerHTML = chars[i];
 		p.appendChild(span_tag);
 	}
+	// adding cursor to the first char
 	current_char = $("#para span")[cursor_index];
 	current_char.classList.add("cursor");
 }
@@ -47,14 +47,27 @@ function cursor_forward(isRight){
 	cursor_index++;
 	current_char = $("#para span")[cursor_index];
 	current_char.classList.add("cursor");
+	// to fetch the next text in advance when half of the previous text is done typing
+	if(cursor_index === Math.round(para_len/2)){
+		getData().then((data) => {  //when promise is resolved
+			para_text = data['content'];
+			console.log(para_text);
+		}).catch((e) => {  //when error occurs
+			para_text = "its some error text";
+			console.log(e.message);
+		});
+	}
 }
 
 document.addEventListener('keydown', (e) => {
-	console.log(e.key);
+	// when current text is done typing
+	if(cursor_index === (para_len - 1)){
+		span_chars(para_text);
+	}
 	// right character is pressed
-	if(e.key === current_char.innerText && !(blacklist_chars.includes(e.key))){
+	else if(e.key === current_char.innerText && !(blacklist_chars.includes(e.key))){
 		isRight = true;
-		cursor_forward(isRight);	
+		cursor_forward(isRight);
 	}
 	// wrong character is pressed
 	else if(!(blacklist_chars.includes(e.key))){
