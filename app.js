2ba866be994f76b56d1398bc30a56fb30a2e8ef0
Aaditya Tile <adi01tile@gmail.com>
2022-11-08 13:34:10 +0530

    popupbox/modal

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const wpm_div = document.getElementById("wpm");
 const accuracy_div = document.getElementById("accuracy");
 const blacklist_chars = ['Shift', 'CapsLock', 'Control', 'Alt', 'Meta', 'Backspace', 'Enter'];
 let current_char, para_text, para_len, cursor_index, typed_chars;
-let isRight = true;
+let isRight = true, wpm, accuracy;
 
 let isStarted = false;
-let initial_min = min = 1;
-let initial_sec = sec = 20;
+let initial_min = min = 0;
+let initial_sec = sec = 1;
 typed_chars = 0;
 
 // error cnt to calculate wpm and accuracy
@@ -36,11 +36,11 @@ getData().then((data) => {  //when promise is resolved
 });
 
 // create span for each char and add it to html paragraph element
-function span_chars(input_text){
+function span_chars(){
 	p.innerHTML = "";
 	cursor_index = 0;
-	para_len = input_text.length;
-	const chars = input_text.split("");
+	para_len = para_text.length;
+	const chars = para_text.split("");
 	for(let i = 0; i < chars.length; i++){
 		let span_tag = document.createElement('span');
 		span_tag.innerHTML = chars[i];
@@ -74,6 +74,13 @@ function cursor_forward(isRight){
 	}
 }
 
+function display_modal(){
+	$('#timeupModal').modal('show'); 
+	console.log(wpm);
+	document.getElementById("modal_wpm").innerHTML = wpm;
+	document.getElementById("modal_accuracy").innerHTML = accuracy;
+}
+
 // to handle timer countdown
 function timer(min, sec){
 	//setInterval() will execute this arrow function snippet repeatedly after every 1000 millisec
@@ -83,6 +90,7 @@ function timer(min, sec){
 			console.log("timer done");
 			document.removeEventListener("keydown", typing_handler);
 			clearInterval(interval_id);
+			display_modal();
 		}
 		else if(sec == 0){
 			sec = 59;
@@ -95,8 +103,8 @@ function timer(min, sec){
 		timer_div.innerHTML = String(min).padStart(2, '0') + " : " + String(sec).padStart(2, '0');
 		// for calculation of wpm and accuracy
 		let time_passed = (initial_min + (initial_sec/60)) - (min + (sec/60));
-		let wpm = Math.round(((typed_chars/5) - uncorrected_errors)/time_passed);
-		let accuracy = Math.round(((typed_chars - total_errors)/typed_chars)*100);
+		wpm = Math.round(((typed_chars/5) - uncorrected_errors)/time_passed);
+		accuracy = Math.round(((typed_chars - total_errors)/typed_chars)*100);
 		wpm_div.innerHTML = wpm;
 		accuracy_div.innerHTML = accuracy;
 	}, 1000);
@@ -111,7 +119,7 @@ function typing_handler(e){
 	}
 	// when current text is done typing
 	if(cursor_index === (para_len - 1)){
-		span_chars(para_text); //we will span the newly fetched text
+		span_chars(); //we will span the newly fetched text
 	}
 	// right character is pressed
 	else if(e.key === current_char.innerText && !(blacklist_chars.includes(e.key))){
