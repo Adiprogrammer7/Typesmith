674d45c1335d15298d325023ee5a45f9491fff48
Aaditya Tile <adi01tile@gmail.com>
2023-02-22 23:51:18 +0530

    db connection working

--- app.js
+++ app.js
@@ -8,6 +8,25 @@ app.get("/", (req, res) => {
     res.render('index');
 });
 
+// Database connection
+const { Client } = require('pg');
+
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'resort_db',
+  password: 'Hack1137@postgres',
+  port: 5432,
+});
+
+client.connect();
+
+client.query('SELECT * FROM customer', (err, res) => {
+  console.log(res.rows);
+  client.end();
+});
+
+
 // to run server on port
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
